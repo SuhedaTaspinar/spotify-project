a03941833fd7bd78915e7ea6b79be411dc6ebfe9
Şüheda <92635912+SuhedaTaspinar@users.noreply.github.com>
2024-07-30 18:16:02 +0300

    player design improved

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
+import React, {useEffect, useRef} from 'react';
+import {Route, Routes, useLocation} from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum.jsx";
+import {albumsData} from "../assets/assets.js";
 
 const Display = () => {
+
+    const disPlayRef = useRef();
+    const location = useLocation();
+    const isAlbum = location.pathname.includes("album");
+    const albumId = isAlbum ? location.pathname.slice(-1) : "";
+    const bgColor = albumsData[Number(albumId)].bgColor;
+
+    useEffect(() => {
+        if (isAlbum){
+            disPlayRef.current.style.background = `linear-gradient(${bgColor}, #121212)`;
+        }
+        else{
+            disPlayRef.current.style.background = `#121212)`;
+        }
+    });
+
     return (
-        <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+        <div ref={disPlayRef} className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
             <Routes>
                 <Route path="/" element={<DisplayHome />} />
                 <Route path="/album/:id" element={<DisplayAlbum />} />
